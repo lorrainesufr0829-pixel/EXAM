d25acb349087dd96abdd17c28449b84758b4de6d
lorrainesufr0829-pixel <lorrainesufr0829@gmail.com>
2026-06-23 19:29:39 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -9,9 +9,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_ID"
+  apiKey: "AIzaSyCA0xyzDy7JmaQEINTvqmXfVzfAh1cNyk0",
+  authDomain: "exam0829-3c850.firebaseapp.com",
+  projectId: "exam0829-3c850",
+  storageBucket: "exam0829-3c850.firebasestorage.app",
+  messagingSenderId: "617125485040",
+  appId: "1:617125485040:web:434b11e219543789882535",
+  measurementId: "G-8TLDDWCC7V"
 };
 
 const app = initializeApp(firebaseConfig);
